e7ff09e02591707484d823a06d5caa088b27d36b
luist23 <00127717@uca.edu.sv>
2019-03-29 17:39:46 -0600

    final

--- app/src/main/java/com/example/pdm_corto_l1/activity_send.java
+++ app/src/main/java/com/example/pdm_corto_l1/activity_send.java
@@ -12,6 +12,7 @@ public class activity_send extends AppCompatActivity {
     private TextView mC1,mC2,mC3,mC4,mC5,mC6,mC7,mC8,mC9,mTotal, mUser,mCorreo;
     private Button mSend;
     private int contador;
+    private String factura;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,23 +32,48 @@ public class activity_send extends AppCompatActivity {
             mC7.setText(mIntent.getStringExtra("p7"));
             mC8.setText(mIntent.getStringExtra("p8"));
             mC9.setText(mIntent.getStringExtra("p9"));
+            factura = coverJson(mIntent);
         }
+
+
 
         mSend.setOnClickListener(v->{
             Intent mIntent2 = new Intent();
             mIntent2.setType("text/plain");
             mIntent2.setAction(Intent.ACTION_SEND);//voy a mandar algo... de tipo texto plan î
-            mIntent2.putExtra(Intent.EXTRA_TEXT,coverJson(mIntent));
+            mIntent2.putExtra(Intent.EXTRA_TEXT,factura);
             startActivity(mIntent2);
         });
 
     }
 
     private String coverJson(Intent i){
+        contador =0;
+        contador += Integer.parseInt(i.getStringExtra("p1"));
+        contador += Integer.parseInt(i.getStringExtra("p2"));
+        contador += Integer.parseInt(i.getStringExtra("p3"));
+        contador += Integer.parseInt(i.getStringExtra("p4"));
+        contador += Integer.parseInt(i.getStringExtra("p5"));
+        contador += Integer.parseInt(i.getStringExtra("p6"));
+        contador += Integer.parseInt(i.getStringExtra("p7"));
+        contador += Integer.parseInt(i.getStringExtra("p8"));
+        contador += Integer.parseInt(i.getStringExtra("p9"));
         String J = "{ \n";
-                J+= "producto1 : " ;
-;
-        return J+= "\n }";
+        J+= "Usuario : "+ i.getStringExtra("user") +"\n";
+        J+= "email : "+ i.getStringExtra("email") + "\n";
+        J+= "producto1 : " + i.getStringExtra("p1") + "\n";
+        J+= "producto2 : " + i.getStringExtra("p2") + "\n";
+        J+= "producto3 : " + i.getStringExtra("p3") + "\n";
+        J+= "producto4 : " + i.getStringExtra("p4") + "\n";
+        J+= "producto5 : " + i.getStringExtra("p5") + "\n";
+        J+= "producto6 : " + i.getStringExtra("p6") + "\n";
+        J+= "producto7 : " + i.getStringExtra("p7") + "\n";
+        J+= "producto8 : " + i.getStringExtra("p8") + "\n";
+        J+= "producto9 : " + i.getStringExtra("p9") + "\n";
+        J+= "total productos : " + contador + "\n";
+
+;       mTotal.setText(String.valueOf(contador));
+        return J+= "}";
     }
 
     private void onLoadContent(){
